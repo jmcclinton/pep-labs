030fba06501ee629df4e79c3fa7424ef36bb9095
jmcclinton <jmcclinton8@gmail.com>
2022-12-21 20:55:18 +0000

    Saved progress on HelloWorld 2022-12-21 20:55:18 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,12 +1,11 @@
-
 public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
     /**
-     * This method must return a hello world String, instead of returning "change me".
-     * A test case will check if the behavior is as expected.
-     * You can use the test results to check for differences.
-     * @return "Hello, world!"
+     * @return
      */
     public String sayHello(){
-        return "change me";
+        return "Hello World!";
+        }
     }
-}
