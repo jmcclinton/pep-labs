3ecf73614870b516de24f6b2ed0122c42e62f1f6
jmcclinton <jmcclinton8@gmail.com>
2022-12-21 21:04:06 +0000

    Saved progress on HelloWorld 2022-12-21 21:04:06 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,9 +1,7 @@
 
 public class HelloWorld {
+
   public static void main(String[] args) {
     System.out.println("Hello, world!");
   }
-    public String sayHello(){
-        return "Hello, World!";
-    }
 }
